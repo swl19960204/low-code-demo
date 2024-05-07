3fcd2cf9d202b97c9cc82263c30883eecee1451e
unknown <1807244619@qq.com>
2024-05-07 22:57:53 +0800

    test: 测试异步组件

--- src/components/Custom/index.js
+++ src/components/Custom/index.js
@@ -1,42 +1,57 @@
 import { defineAsyncComponent } from 'vue'
 
+const components = [
+    // 'CircleShape',
+    // 'Picture',
+    // 'VText',
+    'VButton',
+    // 'Group',
+    // 'RectShape',
+    // 'LineShape',
+    'VTable',
+    // 'VChart',
+]
 
 export const registerCustomComp = {
     install: (app) => {
         // import.meta.glob是vite的新api
-        const components = import.meta.glob("./*/**.vue", { eager: true });
-        console.log("🚀 ~ components:", components)
-        // 遍历组件模块实现自动注册
-        for (const [path, component] of Object.entries(components)) {
-            console.log("🚀 ~ path:", path)
+        // const components = import.meta.glob("./*/**.vue");
+        // console.log("🚀 ~ components:", components)
+        // // 遍历组件模块实现自动注册
+        // for (const [path, component] of Object.entries(components)) {
+        //     console.log("🚀 ~ path:", path)
 
-            // // 使用正则表达式匹配路径中的组件前缀和文件名
-            // const match = path.match(/\/([^/]+)\/(V[^/]+)\//);
-            // if (!match) {
-            //     throw new Error('路径格式不正确或未找到组件前缀');
-            // }
-            // // 提取组件前缀和文件名
-            // const [_, componentName, prefix] = match;
-            // // 从路径中提取原始文件名
-            // const originalFileName = path.split('/').pop();
-            // // 分离文件扩展名
-            // const baseName = originalFileName.split('.')[0];
-            // if (baseName === 'index') {
-            //     app.component(prefix, defineAsyncComponent(component));
-            // } else {
-            //     app.component(`${prefix}${baseName}`, defineAsyncComponent(component));
-            // }
-            const folderNameMatch = path.match(/\.\/(.*?)\/.+/);
-            const attrNameMatch = path.match(/\/(\w+).vue$/);
-            const folderName = folderNameMatch ? folderNameMatch[1] : null;
-            const componentName = attrNameMatch ? attrNameMatch[1] : null;
+        //     // // 使用正则表达式匹配路径中的组件前缀和文件名
+        //     // const match = path.match(/\/([^/]+)\/(V[^/]+)\//);
+        //     // if (!match) {
+        //     //     throw new Error('路径格式不正确或未找到组件前缀');
+        //     // }
+        //     // // 提取组件前缀和文件名
+        //     // const [_, componentName, prefix] = match;
+        //     // // 从路径中提取原始文件名
+        //     // const originalFileName = path.split('/').pop();
+        //     // // 分离文件扩展名
+        //     // const baseName = originalFileName.split('.')[0];
+        //     // if (baseName === 'index') {
+        //     //     app.component(prefix, defineAsyncComponent(component));
+        //     // } else {
+        //     //     app.component(`${prefix}${baseName}`, defineAsyncComponent(component));
+        //     // }
+        //     const folderNameMatch = path.match(/\.\/(.*?)\/.+/);
+        //     const attrNameMatch = path.match(/\/(\w+).vue$/);
+        //     const folderName = folderNameMatch ? folderNameMatch[1] : null;
+        //     const componentName = attrNameMatch ? attrNameMatch[1] : null;
 
-            if (componentName === 'index') {
-                app.component(folderName, defineAsyncComponent(component));
-            } else {
-                app.component(folderName + componentName, defineAsyncComponent(component));
-            }
-        }
+        //     if (componentName === 'index') {
+        //         app.component(folderName, defineAsyncComponent(component));
+        //     } else {
+        //         app.component(folderName + componentName, defineAsyncComponent(component));
+        //     }
+        // }
+        components.forEach(key => {
+            app.component(key, defineAsyncComponent(() => import(`./${key}/index.vue`)))
+            app.component(key + 'Attr', defineAsyncComponent(() => import(`./${key}/Attr.vue`)))
+        })
 
 
 
