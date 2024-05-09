9ca772f0738707a27f0d43c88dfb7c4159e51f05
unknown <1807244619@qq.com>
2024-05-09 21:27:15 +0800

    test: 测试异步组件

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
-
+const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/low-code-demo',
@@ -26,7 +26,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
